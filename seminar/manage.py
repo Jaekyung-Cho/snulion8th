c3e8aba82e2f27b45a68d47e78a9ac247e094bba
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2020-06-29 19:53:23 +0900

    temp

--- seminar/manage.py
+++ seminar/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/Users/jackyoung96/opt/anaconda3/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -13,9 +13,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
